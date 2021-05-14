72d3ae0f112db790682f8972bc4f1bc59674f1eb
Raphael <raphael.correa@bennu.com.br>
2021-05-13 23:27:15 -0300

    SLICK

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { GlobalStyle } from "./theme/GlobalStyles";
 import Router from "./pages/Router";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { ThemeProvider } from "styled-components";
 import theme from "./theme/theme";
